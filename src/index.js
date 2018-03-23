777569e92f91b16803b16293c3487e7c22a66937
“Terris“ <terris.kremer@gmail.com>
2018-03-23 12:05:45 -0600

    config redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
+// Components
+import App from './App';
+// Actions & Reducers
+import reducers from './reducers';
+// Create Store with Middleware
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducers)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <App />
+    </div>
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
